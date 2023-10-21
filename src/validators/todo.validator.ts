bf9313d0733d069d221fbcc0af264f93ef3f2e82
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2023-10-21 09:32:33 +0530

    docs(validators): Improve error messages

--- src/validators/todo.validator.ts
+++ src/validators/todo.validator.ts
@@ -4,13 +4,13 @@ import validate from '../util/validate';
 const createTodoValidator = validate([
   body('title')
     .isString()
-    .withMessage('Title must be of type string.')
+    .withMessage('Title must be defined of type string.')
     .trim()
     .notEmpty()
     .withMessage('Todo title is required and cannot be empty.'),
   body('description')
     .isString()
-    .withMessage('Desciption must be of type string.'),
+    .withMessage('Desciption must be defined of type string.'),
 ]);
 
 export { createTodoValidator };
