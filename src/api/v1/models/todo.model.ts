dfe30721d2e37f74af0c16955b0291d1d98a5d67
ChiragWadhwani <wadhwanichirag94@gmail.com>
2023-10-26 00:36:16 +0530

    fix: apiError assets: asyncHandler

--- src/api/v1/models/todo.model.ts
+++ src/api/v1/models/todo.model.ts
@@ -1,6 +1,6 @@
 import { sequelize } from '../db';
 import { DataTypes } from 'sequelize';
-import { asyncHandller } from '../../../util/asyncHandler';
+import { asyncHandller } from '../util/asyncHandler';
 
 const Todo = sequelize.define('Todo', {
   title: {
@@ -54,52 +54,5 @@ async function deleteById(id: number): Promise<number> {
     return deletedRows;
   }, 'Failed to deleteById: ');
 }
-
-// This Is the old version of code without asyncHandler implementation
-
-// async function create(title: string, description: string): Promise<object> {
-//   try {
-//     const todo = await Todo.create({ title, description });
-//     const jsonData = await todo.toJSON();
-//     return jsonData;
-//   } catch (error) {
-//     console.error('Failed to create todo:', error);
-//     throw error;
-//   }
-// }
-
-// async function getAll(): Promise<object[]> {
-//   try {
-//     const todo = await Todo.findAll();
-//     return todo;
-//   } catch (error) {
-//     console.error('Failed to getAll todos:', error);
-//     throw error;
-//   }
-// }
-
-// async function deleteAll(): Promise<number> {
-//   try {
-//     const todo = await Todo.count();
-//     if (todo > 0) {
-//       await Todo.truncate();
-//       return todo;
-//     }
-//     return 0;
-//   } catch (error) {
-//     console.error('Failed to deleteAll todos', error);
-//     throw error;
-//   }
-// }
-
-// async function deleteById(id: number): Promise<number> {
-//   try {
-//     const todo = await Todo.destroy({ where: { id } });
-//     return todo;
-//   } catch (error) {
-//     console.error('Failed to deleteById: ', error);
-//     throw error;
-//   }
-// }
 
 export { create, getAll, deleteAll, deleteById };
